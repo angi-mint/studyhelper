e6b846bd2c46b52faa78e0c480b56e8a8c5a3f7e
Angi Mint <68991185+angi-mint@users.noreply.github.com>
2024-04-21 21:15:59 +0200

    added event listener to startBtn
    Signed-off-by: Angi Mint <68991185+angi-mint@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 // Imports use relative file paths or Node.js package names
 import './styles/styles.css';
 import {initNav} from "./utility.ts";
+import {startTimer} from "./timer.ts";
+import {startBtnEl} from "./dom-utils.ts";
 
 initNav();
+
+startBtnEl.addEventListener("click", startTimer);
