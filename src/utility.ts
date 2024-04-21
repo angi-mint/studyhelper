3ea89d4044214cf2b081cf4abed9a78088fcfeaa
Angi Mint <68991185+angi-mint@users.noreply.github.com>
2024-04-21 16:02:33 +0200

    implementing open and close functionality for navigation
    Signed-off-by: Angi Mint <68991185+angi-mint@users.noreply.github.com>

---
+++ src/utility.ts
@@ -0,0 +1,13 @@
+import {closeBtnEl, navigationEL, openBtnEl} from "./dom-utils.ts";
+
+function initNav() {
+    openBtnEl.addEventListener("click", function () {
+        navigationEL.style.width = "200px";
+
+    })
+    closeBtnEl.addEventListener("click", function () {
+        navigationEL.style.width = "0"
+    })
+}
+
+export { initNav }
