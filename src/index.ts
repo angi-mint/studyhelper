c0393a0920bac85b72d4bd0e21c8ba1d2bffa9b8
Angi Mint <68991185+angi-mint@users.noreply.github.com>
2024-04-21 21:20:23 +0200

    added event listener to pauseBtn
    Signed-off-by: Angi Mint <68991185+angi-mint@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 // Imports use relative file paths or Node.js package names
 import './styles/styles.css';
 import {initNav} from "./utility.ts";
-import {startTimer} from "./timer.ts";
-import {startBtnEl} from "./dom-utils.ts";
+import {pauseTimer, startTimer} from "./timer.ts";
+import {pauseBtnEl, startBtnEl} from "./dom-utils.ts";
 
 initNav();
 
 startBtnEl.addEventListener("click", startTimer);
+pauseBtnEl.addEventListener("click", pauseTimer);
