5400851d8ca9aeee9e9b30f3290002d64a411220
Angi Mint <68991185+angi-mint@users.noreply.github.com>
2024-04-21 20:47:23 +0200

    exporting controller buttons and timer elements
    Signed-off-by: Angi Mint <68991185+angi-mint@users.noreply.github.com>

--- src/dom-utils.ts
+++ src/dom-utils.ts
@@ -2,6 +2,16 @@ const navigationEL = document.querySelector("#nav-side") as HTMLElement;
 const openBtnEl = document.querySelector("#open-btn") as HTMLButtonElement;
 const closeBtnEl = document.querySelector("#close-btn") as HTMLButtonElement;
 
+const startBtnEl = document.querySelector(".btn-start") as HTMLButtonElement;
+const pauseBtnEl = document.querySelector(".btn-pause") as HTMLButtonElement;
+const stopBtnEl = document.querySelector(".btn-stop") as HTMLButtonElement;
+const timerMinEl = document.querySelector("#timer-minutes") as HTMLParagraphElement;
+const timerSecEl = document.querySelector("#timer-seconds") as HTMLParagraphElement;
+
+const timeMin = Number(timerMinEl.textContent);
+const timeSec = Number(timerSecEl.textContent);
 export {
-    navigationEL, openBtnEl, closeBtnEl
+    navigationEL, openBtnEl, closeBtnEl,
+    startBtnEl, pauseBtnEl, stopBtnEl,
+    timerMinEl, timerSecEl, timeMin, timeSec
 }
