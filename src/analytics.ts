94ff6e2f68505524bb0776329d8ce1a2538e2ece
Angi Mint <68991185+angi-mint@users.noreply.github.com>
2024-04-23 14:29:12 +0200

    functions to display the charts and text on the site
    Signed-off-by: Angi Mint <68991185+angi-mint@users.noreply.github.com>

--- src/analytics.ts
+++ src/analytics.ts
@@ -1,4 +1,21 @@
 import './styles/styles.css';
 import { initNav } from "./common/utility.ts";
 
+import { getCategoryTimes, getSubjectTimes, totalTimeSpent } from "./common/localstorage.ts";
+import { createPieChart } from "./analytics/chart.ts";
+import { chartCategoryEl, chartSubjectEl, textCategoryEl, textSubjectEl, textTotalEl } from "./common/dom-utils.ts";
+
 initNav();
+
+let times: number[] = getCategoryTimes();
+const labels: string[] = ['lecture', 'project', 'studying']
+
+createPieChart(chartCategoryEl,textCategoryEl, times, labels)
+
+let subjectTimes: number[] = getSubjectTimes();
+const subjects: string[] = ['t2', 't3', 'u1', 'd1', 'p1'];
+
+const totalTime = totalTimeSpent();
+if (totalTime !== 0) textTotalEl.innerHTML = `You have spent a total of ${totalTime} hours studying.`
+
+createPieChart(chartSubjectEl, textSubjectEl, subjectTimes, subjects)
