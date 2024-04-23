a0a4e8d624513b5f0f5a4f3426f0ce31d280497e
Angi Mint <68991185+angi-mint@users.noreply.github.com>
2024-04-23 07:50:09 +0200

    exporting category and subject elements
    Signed-off-by: Angi Mint <68991185+angi-mint@users.noreply.github.com>

--- src/dom-utils.ts
+++ src/dom-utils.ts
@@ -5,13 +5,18 @@ const closeBtnEl = document.querySelector("#close-btn") as HTMLButtonElement;
 const startBtnEl = document.querySelector(".btn-start") as HTMLButtonElement;
 const pauseBtnEl = document.querySelector(".btn-pause") as HTMLButtonElement;
 const stopBtnEl = document.querySelector(".btn-stop") as HTMLButtonElement;
+
 const timerMinEl = document.querySelector("#timer-minutes") as HTMLParagraphElement;
 const timerSecEl = document.querySelector("#timer-seconds") as HTMLParagraphElement;
-
 const timeMin = Number(timerMinEl.textContent);
 const timeSec = Number(timerSecEl.textContent);
+
+const categoryEl = document.querySelector("#category") as HTMLSelectElement;
+const subjectEl = document.querySelector("#subject") as HTMLSelectElement;
+
 export {
     navigationEL, openBtnEl, closeBtnEl,
     startBtnEl, pauseBtnEl, stopBtnEl,
-    timerMinEl, timerSecEl, timeMin, timeSec
+    timerMinEl, timerSecEl, timeMin, timeSec,
+    categoryEl, subjectEl
 }
