731b216cc2daac049b09f791773fa39f028e9f5f
Angi Mint <68991185+angi-mint@users.noreply.github.com>
2024-04-26 22:11:19 +0200

    calling new function updateLinks
    Signed-off-by: Angi Mint <68991185+angi-mint@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 // Imports use relative file paths or Node.js package names
 import './styles/styles.css';
 import { initNav } from "./common/utility.ts";
-import { startTimer, pauseTimer, stopTimer } from "./timer/timer.ts";
-import { startBtnEl, pauseBtnEl, stopBtnEl } from "./common/dom-utils.ts";
+import { startTimer, pauseTimer, stopTimer, updateLinks } from "./timer/timer.ts";
+import { startBtnEl, pauseBtnEl, stopBtnEl, subjectEl } from "./common/dom-utils.ts";
 
 initNav();
+updateLinks();
 
 startBtnEl.addEventListener("click", startTimer);
 pauseBtnEl.addEventListener("click", pauseTimer);
 stopBtnEl.addEventListener("click", stopTimer);
+subjectEl.addEventListener("change", updateLinks)
