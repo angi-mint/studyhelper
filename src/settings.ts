ed90c62b35ce0c01ed6cf485df164fe7588e393d
Angi Mint <68991185+angi-mint@users.noreply.github.com>
2024-05-05 20:59:58 +0200

    display current categories and subjects
    Signed-off-by: Angi Mint <68991185+angi-mint@users.noreply.github.com>

--- src/settings.ts
+++ src/settings.ts
@@ -1,16 +1,19 @@
 import './styles/styles.css';
 
-import {categoryForm, categoryInput, subjectForm, subjectInput} from "./common/dom-utils.ts";
+import {categoryForm, categoryInput, categoryList, subjectForm, subjectInput, subjectList} from "./common/dom-utils.ts";
 import {settings} from "./common/localstorage.ts";
 import {initNav} from "./common/utility.ts";
 
 initNav();
+updateCategory();
+updateSubject();
 categoryForm.addEventListener('submit', function(event: Event) {
     event.preventDefault();
 
     const category: string = categoryInput.value;
     settings("category", category);
-    categoryInput.value = "";
+    categoryForm.reset();
+    updateCategory();
 });
 
 subjectForm.addEventListener('submit', function(event: Event) {
@@ -18,5 +21,28 @@ subjectForm.addEventListener('submit', function(event: Event) {
 
     const subject: string = subjectInput.value;
     settings("subject", subject);
-    subjectInput.value = "";
+    subjectForm.reset();
+    updateSubject();
 });
+
+function updateCategory() {
+    const categories: Array<string> = settings("category");
+
+    categoryList.innerHTML = "";
+    categories.forEach((category: string) => {
+        const li: HTMLLIElement = document.createElement("li");
+        li.innerText = category;
+        categoryList.appendChild(li);
+    });
+}
+
+function updateSubject() {
+    const subjects: Array<string> = settings("subject");
+
+    subjectList.innerHTML = "";
+    subjects.forEach((subject: string) => {
+        const li: HTMLLIElement = document.createElement("li");
+        li.innerText = subject;
+        subjectList.appendChild(li);
+    });
+}
